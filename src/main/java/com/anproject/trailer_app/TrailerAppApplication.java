5403295e75b96a559ed06d9209cded9be8deb781
hakan-atalay <hakannatalayyy@gmail.com>
2024-09-17 13:33:38 +0300

    Add configuration for Jasypt encryption

--- src/main/java/com/anproject/trailer_app/TrailerAppApplication.java
+++ src/main/java/com/anproject/trailer_app/TrailerAppApplication.java
@@ -3,7 +3,11 @@ package com.anproject.trailer_app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+
+
 @SpringBootApplication
+@EnableEncryptableProperties
 public class TrailerAppApplication {
 
 	public static void main(String[] args) {
